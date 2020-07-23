799637c41ca18dbba8d0ef7353ece078fddde932
Crowdbotics <team@crowdbotics.com>
2020-07-23 18:17:15 +0000

    Update models

--- backend/rpa_19128/wsgi.py
+++ backend/rpa_19128/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'rpa_19128.settings')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "rpa_19128.settings")
 
 application = get_wsgi_application()
